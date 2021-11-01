6bc56647a8b430cf69d030dd74d1b486fb73ec3f
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-11-01 15:26:48 -0700

    created structure

---
+++ Program/src/employeeMain.c
@@ -0,0 +1,70 @@
+//gcc employeeMain.c employeeTable.c employeeOne.c
+//gcc employeeMain.c employeeTable.c employeeTwo.c
+#include <string.h>
+#include <stdlib.h>
+#include "../include/employee.h"
+
+int main(void)
+{
+    //defined in employeeOne.c or employeeTwo.c
+    PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
+    PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind);
+    PtrToEmployee searchEmployeeByPhoneNumber(const Employee table[], int sizeTable, char * phoneToFind);
+    PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double nameToFind);
+
+    //defined in employeeTable.c
+    extern Employee EmployeeTable[];
+    extern const int EmployeeTableEntries;
+
+    PtrToEmployee matchPtr; //Declaration
+    
+    //Example not found
+    matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045);
+    if (matchPtr != NULL)
+        printf("Employee ID %d is in the record %d\n", 1045, matchPtr - EmployeeTable);
+    else 
+        printf("Employee ID is NOT found in the record\n");
+
+    //Example found
+    matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat");
+    if (matchPtr != NULL)
+        printf("Employee %s is in record %d\n", "Tony Bobcat", matchPtr - EmployeeTable);
+    else
+        printf("Employee %s is NOT found in the record\n", "Tony Bobcat");
+
+
+    //Example of Match
+    double salary = 4.50;
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, salary);
+    if (matchPtr != NULL)
+        printf("Employee with a salary of %.2lf is in record %d.\n", salary, matchPtr - EmployeeTable);
+    else
+        printf("Employee with a salary of %.2lf is NOT found in the record.\n", salary);
+
+    //Example of No match
+    salary = 5.65;
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, salary);
+    if (matchPtr != NULL)
+        printf("Employee with a salary of %.2lf is in record %d.\n", salary, matchPtr - EmployeeTable);
+    else
+        printf("Employee with a salary of %.2lf is NOT found in the record.\n", salary);
+
+
+    //Example of Match
+    char * phoneNumber = "909-555-1235";
+    matchPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, phoneNumber);
+    if (matchPtr != NULL)
+        printf("Employee with phone number '%s' is in record %d.\n", phoneNumber, matchPtr - EmployeeTable);
+    else
+        printf("Employee with phone number '%s' is NOT found in the record.\n", phoneNumber);
+
+    //Example of No match
+    phoneNumber = "714-259-7591";
+    matchPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, phoneNumber);
+    if (matchPtr != NULL)
+        printf("Employee with phone number '%s' is in record %d.\n", phoneNumber, matchPtr - EmployeeTable);
+    else
+        printf("Employee with phone number '%s' is NOT found in the record.\n", phoneNumber);
+
+    return EXIT_SUCCESS;
+}
---
+++ Program/src/employeeTwo.c
@@ -0,0 +1,72 @@
+#include <string.h>
+#include "../include/employee.h"
+
+//NOTE: 5 functions have been defined below
+
+//ptr: points to table to be searched
+//tableSize: size of tehe table
+//targetPtr: points to value that will be searched for in table
+//functionPtr: points to comparison function  below and will perform the associated check
+
+
+//what does settinga  function to static do? The functions can only be used within in this file/class
+//functionPtr is the address of the function just as the name
+//()(The second set of parentheses is the arguments of the function cell)
+
+static PtrToEmployee searchEmployeeTable (PtrToConstEmployee ptr, int tableSize, const void *targetPtr,
+                                        int (*functionPtr)(const void *, PtrToConstEmployee))
+{ //static, for functions = only usable in this file. Basically private.
+    PtrToConstEmployee endPtr = ptr + tableSize;
+
+    for(; ptr < endPtr; ptr++)
+    {
+        if ((*functionPtr)(targetPtr, ptr) == 0)    //compareEmployeeNumber(&1045, TableRow)
+            return (PtrToEmployee) ptr;
+    }
+    return NULL;
+}
+
+//The functionPtr will point to one of these comparison functions to perform a check
+static int compareEmployeeNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return * (long *) targetPtr != tableValuePtr->number;
+}
+
+static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return strcmp((char *) targetPtr, tableValuePtr->name);
+}
+
+
+static int compareEmployeeByPhoneNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return strcmp((char *) targetPtr, tableValuePtr->phone);
+}
+
+static int compareEmployeeBySalary(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return * (double *) targetPtr != tableValuePtr->salary;
+}
+
+
+//These are called wrappers. These functions are what you will use in your main.c
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number)
+{   //pass the ADDRESS of number
+    return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber);
+}
+
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name)
+{
+    return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+
+
+PtrToEmployee searchEmployeeByPhoneNumber(PtrToConstEmployee ptr, int size, char* phone)
+{
+    return searchEmployeeTable(ptr, size, phone, compareEmployeeByPhoneNumber);
+}
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary)
+{   //pass the ADDRESS of salary
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeBySalary);
+}
